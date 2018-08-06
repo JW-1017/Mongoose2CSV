297e0a449aa1d85b7f5c895e5f3081e248ff193a
steve <wldndsla8@gmail.com>
2018-08-06 10:20:40 +0900

    column 명을 미리 넣어주는 것으로 변경

--- mongoose2CSV.js
+++ mongoose2CSV.js
@@ -1,14 +1,14 @@
 /*
-   # Mongoose2CSV
-NodeJ.S 에서 mongoose(몽고DB)의 데이터들을 CSV파일로 뽑아내는 모듈 (log데이터 등을 뽑아낼때 유용)
+   Node.JS Mongoose to CSV File
+   원하는 인자만, 원하는 Date 형태로 (moment 이용) 파일 생성
+/* Copyright (C) 2018.08.05 by Son J.W*/
 
-Node.JS Mongoose to CSV File
-원하는 인자만, 원하는 Date 형태로 (moment 이용) 파일 생성
+// -- 인자 설명 5개 --
+// 파일이름, response, collection 배열, 미리 넣어둘 string, 출력할 스키마 배열(조인 일경우 joinProperty생성)
+// 배열일 경우 배열 속 배열은 지원x 배열속 ref 1번만 지원 arrProperty
 
--- 인자 설명 5개 --
-파일이름, response(res), collection 배열, 미리 넣어둘 string, 출력할 스키마 배열
-조인 일 경우 연속 적인 joinProperty배열 활용, 배열 일 경우 arrProperty배열 활용
-titleSpace는 열의 이름들을 위해 정함(array일 경우 array크기만큼 추가 셀을 이용하므로 titleSpace를 통해 추가 셀 배정)
+/*
+preString을 통해 column명을 포함하여야 함
 
 join일 경우 반드시 원하는 schema를 포함한 joinProperty속성을 포함한 배열을
 Array일 경우 원하는 schema를 포함한 arrProperty속성을 포함한 배열을 넣어야 하고
@@ -16,21 +16,13 @@ Array일 경우 원하는 schema를 포함한 arrProperty속성을 포함한 배
 추가로 Date일 경우 원하는 타입을 dateFormat에 넣어주어야 함(안 넣으면 원래 형식대로 출력)
 
 예시:
-mongoose2CSV.mongoose2CSV('noticeboard.csv', res, data, "",[{name:'numId'}, {name:'title'}, {name:'body'}, 
-      {name:'author', joinProperty:[{name: 'nickname'}, {name: 'email'}], titleSpace:2}, {name:'createdAt', dateFormat:'YYYY[-]MM[-]DD HH[:]mm[:]ssZ'},
-      {name:'comments', arrProperty:[{name:'author', joinProperty:[{name: 'nickname'}, {name: 'email'}]},{name: 'body'}, {name:'createdAt', dateFormat:'YYYY[-]MM[-]DD HH[:]mm[:]ssZ'}], titleSpace:2}]);
+  var str_temp = "<post>\nnumId,title,body,*author.nickname,*author.email,createdAt,comments[],,"
 
-
--한계 
-
-배열 일 경우에 대해 배열 속 배열은 지원x
-배열 속 속성에 대해 join은 1번만 지원
-join된 속성에서 array가 있는 경우 지원x
-
-(내용 모두 출력은 하게 구현 할 수 있지만 셀에서 보여지는 모습의 문제 때문에 보류 중)
-
-/* Copyright (C) 2018 by Son J.W*/
-
+  mongoose2CSV.mongoose2CSV('noticeboard.csv', res, data, str_temp,[{name:'numId'}, {name:'title'}, {name:'body'}, 
+    {name:'author', joinProperty:[{name: 'nickname'}, {name: 'email'}]}, {name:'createdAt', dateFormat:'YYYY[-]MM[-]DD HH[:]mm[:]ssZ'},
+    {name:'comments', arrProperty:[{name:'author', joinProperty:[{name: 'nickname'}, {name: 'email'}]},{name: 'body'}, {name:'createdAt', dateFormat:'YYYY[-]MM[-]DD HH[:]mm[:]ssZ'}]}]);
+  });
+*/
 var mime = require('mime');
 var Readable = require('stream').Readable;
 var moment = require('moment');
@@ -48,19 +40,21 @@ exports.mongoose2CSV = function(fileName, response, collections, preString, sche
   var s_len = schema.length;
   
   // for UTF-8 Support + modelname
-  str.push("\uFEFF<" + preString + "\n");
+  str.push("\uFEFF" + preString + "\n");
 
-  str.push(collections[0].__proto__.constructor.modelName + ">\n");
+  //str.push(collections[0].__proto__.constructor.modelName + ">\n");
 
   // schema name display  * : join, [] : array  join and array component in one cell (because of join join join(array) etc...)
-  var str_temp = "";
-  for(var i = 0; i < s_len; i++){
-    (schema[i].joinProperty != null) ? str_temp += "*" + schema[i].name + ",".repeat(schema[i].titleSpace-1) : str_temp += schema[i].name;
-    (schema[i].arrProperty != null) ? str_temp += '[],'  + ",".repeat(schema[i].titleSpace-1) : str_temp += ','
+  if(preString == null){
+    var str_temp = "";
+    for(var i = 0; i < s_len; i++){
+      (schema[i].joinProperty != null) ? str_temp += "*" + schema[i].name + ",".repeat(schema[i].titleSpace-1) : str_temp += schema[i].name;
+      (schema[i].arrProperty != null) ? str_temp += '[],'  + ",".repeat(schema[i].titleSpace-1) : str_temp += ','
+    }
+    str_temp += '\n';
+    str.push(str_temp);
   }
-  str_temp += '\n';
-  str.push(str_temp);
-  
+
   var c_len = collections.length;
   for(var i = 0; i < c_len; i++){
     for(var j = 0; j < s_len; j++){
@@ -99,7 +93,7 @@ exports.mongoose2CSV = function(fileName, response, collections, preString, sche
       }
 
       else if(schema[j].joinProperty != null){
-        processJoin(_collection, schema[j].joinProperty, "", str);
+        processJoin(_collection, schema[j].joinProperty, str);
       }
       //
       else {
@@ -125,24 +119,24 @@ exports.processString = function(p_string){
 };
 
 // 연속된 조인일 경우 .A.B.C: value 이런식으로 표기
-function processJoin(_collection, joinProperty, columnName, str){
+function processJoin(_collection, joinProperty, str){
   var len = joinProperty.length;
   for(var i = 0; i < len; i++){
     // for Join 계속되는 join을 해결 가능
     if(joinProperty[i].joinProperty != null){
-      processJoin(_collection[joinProperty[i].name], joinProperty[i].joinProperty, columnName+ ".*" + joinProperty[i].name, str);
+      processJoin(_collection[joinProperty[i].name], joinProperty[i].joinProperty, str);
     }
     //
     else {
       if(typeof(joinProperty[i].name) == 'string'){
-        str.push(processString(columnName+ "." + joinProperty[i].name + ": " + _collection[joinProperty[i].name]) + ',');
+        str.push(processString(_collection[joinProperty[i].name]) + ',');
         continue;
       }
       if(joinProperty[i].dateFormat != null){
-        str.push(columnName+ "." + joinProperty[i].name + ": " + moment(_collection[joinProperty[i].name]).format(joinProperty[i].dateFormat) + ',');
+        str.push(moment(_collection[joinProperty[i].name]).format(joinProperty[i].dateFormat) + ',');
         continue;
       }
-      str.push(columnName+ "." + joinProperty[i].name + ": " + _collection[joinProperty[i].name] + ',');
+      str.push(_collection[joinProperty[i].name] + ',');
     }
   }
 }
